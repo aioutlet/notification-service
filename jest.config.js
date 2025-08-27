cd886a50d515c41d200cd85da1b46eb1410de322
prasadhonrao <honrao.prasad@gmail.com>
2025-08-27 18:58:57 +0100

    chore: Remove unused Jest configuration and test files

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts', // Exclude server startup file
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  testTimeout: 10000,
+  verbose: true,
+  // Module name mapping for path aliases and .js extensions
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  // Environment variables for testing
+  setupFiles: ['<rootDir>/tests/env.ts'],
+  // Make Jest globals available
+  injectGlobals: true,
+  // Enable ESM support
+  extensionsToTreatAsEsm: ['.ts'],
+};
